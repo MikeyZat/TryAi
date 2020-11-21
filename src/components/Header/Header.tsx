35f74057c290d632443cea4440289bac4d3a901b
Mikołaj Zatorski <41756225+MikeyZat@users.noreply.github.com>
2020-11-21 11:35:41 +0100

    Globalize styles with less (#4)

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Layout } from 'antd';
 import { useTranslation } from 'react-i18next';
+import './header.less';
 
 const MainHeader = () => {
   const { t } = useTranslation('common');
   return (
     <Layout.Header>
-      <div style={{ color: '#fff' }}>{t('components.common.appTitle')}</div>
+      <div className="logo">{t('components.common.appTitle')}</div>
     </Layout.Header>
   );
 };
